019db2fce4e4046210f17283126b606afe77d2bf
satyaandm <satyaandm@gmail.com>
2021-04-10 14:45:19 -0400

    country page css

--- scripts/country.js
+++ scripts/country.js
@@ -2,11 +2,11 @@ let currentUrl = window.location.search;
 let currentCountry = currentUrl.slice(9);
 
 let options = {
-  method: 'GET',
-  mode: 'cors',
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-  },
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+    },
 };
 
 let urlConfirmed = "https://api.covid19api.com/total/country/" + currentCountry + "/status/confirmed";
@@ -20,45 +20,45 @@ let dates = [];
 let countryName;
 
 async function fetchData() {
-  try {
-    let responseConfirmed = await fetch(urlConfirmed, options);
-    let responseRecovered = await fetch(urlRecovered, options);
-    let responseDeaths = await fetch(urlDeaths, options);
-    
-    let dataConfirmed = await responseConfirmed.json();
-    let dataDeaths = await responseDeaths.json();
-    let dataRecovered = await responseRecovered.json();
+    try {
+        let responseConfirmed = await fetch(urlConfirmed, options);
+        let responseRecovered = await fetch(urlRecovered, options);
+        let responseDeaths = await fetch(urlDeaths, options);
 
-    countryName = dataConfirmed[0].Country;
-    dataConfirmed.forEach(e => {
-      cases.push(e.Cases);
-      dates.push(e.Date.slice(0, 10));
-    })
-    dataDeaths.forEach(e => {
-      deaths.push(e.Cases);
-    })
-    dataRecovered.forEach(e => {
-      recovered.push(e.Cases);
-    })
-  } catch (error) {
-    // window.location.replace('index.html');
-    alert('Error Loading Page.');
-    console.log(error);
-  } finally {
-    totalGraph();
-    dailyCasesGraph();
-    dailyDeathsGraph();
-    dailyRecoveredGraph();
-    drawTable();
-    setPageTitle();
-  }
+        let dataConfirmed = await responseConfirmed.json();
+        let dataDeaths = await responseDeaths.json();
+        let dataRecovered = await responseRecovered.json();
+
+        countryName = dataConfirmed[0].Country;
+        dataConfirmed.forEach(e => {
+            cases.push(e.Cases);
+            dates.push(e.Date.slice(0, 10));
+        })
+        dataDeaths.forEach(e => {
+            deaths.push(e.Cases);
+        })
+        dataRecovered.forEach(e => {
+            recovered.push(e.Cases);
+        })
+    } catch (error) {
+        // window.location.replace('index.html');
+        alert(`Error Loading Page: ${error}`);
+        console.log(error);
+    } finally {
+        totalGraph();
+        dailyCasesGraph();
+        dailyDeathsGraph();
+        dailyRecoveredGraph();
+        drawTable();
+        setPageTitle();
+    }
 }
 
 function setPageTitle() {
-  const ctx = document.getElementById('country-title');
-  const length = cases.length - 1;
-  let title = `${countryName}: Cases: ${cases[length]}  Deaths: ${deaths[length]}  Recovered: ${recovered[length]}`;
-  ctx.innerText = title;
+    const ctx = document.getElementById('country-title');
+    const length = cases.length - 1;
+    let title = `${countryName}: Cases: ${cases[length]}  Deaths: ${deaths[length]}  Recovered: ${recovered[length]}`;
+    ctx.innerText = title;
 }
 
 function dailyCasesGraph() {
@@ -76,6 +76,10 @@ function dailyCasesGraph() {
     let myBarChart = new Chart(ctx, {
         type: 'bar',
         data: data,
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+        },
     })
 }
 
@@ -94,6 +98,10 @@ function dailyDeathsGraph() {
     let myBarChart = new Chart(ctx, {
         type: 'bar',
         data: data,
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+        },
     })
 }
 
@@ -112,51 +120,58 @@ function dailyRecoveredGraph() {
     let myBarChart = new Chart(ctx, {
         type: 'bar',
         data: data,
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+        },
     })
 }
 
 function totalGraph() {
-  const ctx = document.getElementById('total-graph').getContext('2d');
-  const data = {
-    labels: dates,
-    datasets: [{
-      label: `Cases`,
-      data: cases,
-      fill: false,
-      borderColor: 'rgba(0, 200, 0, 0.5)',
-      tension: 1,
-    },
-    {
-      label: `Deaths`,
-      data: deaths,
-      fill: false,
-      borderColor: 'rgba(200, 0, 0, 0.5)',
-      tension: 0.1,
-    },
-    {
-      label: `Recovered`,
-      data: recovered,
-      fill: false,
-      borderColor: 'rgba(0, 0,200, 0.5)',
-      tension: 0.1,
-    },
-    ]
-  }
-  let myLineChart = new Chart(ctx, {
-    type: 'line',
-    data: data,
-    options: { responsive: false },
-  })
+    const ctx = document.getElementById('total-graph').getContext('2d');
+    const data = {
+        labels: dates,
+        datasets: [{
+                label: `Cases`,
+                data: cases,
+                fill: false,
+                borderColor: 'rgba(0, 200, 0, 0.5)',
+                tension: 1,
+            },
+            {
+                label: `Deaths`,
+                data: deaths,
+                fill: false,
+                borderColor: 'rgba(200, 0, 0, 0.5)',
+                tension: 0.1,
+            },
+            {
+                label: `Recovered`,
+                data: recovered,
+                fill: false,
+                borderColor: 'rgba(0, 0,200, 0.5)',
+                tension: 0.1,
+            },
+        ]
+    }
+    let myLineChart = new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+        },
+    })
 }
 
 function drawTable() {
-  let html = ``;
-  let result = document.getElementById("result");
-  for (let x = 0; x < cases.length; x++) {
-    if ((cases[x] + deaths[x] + recovered[x]) == 0) {
-      continue;
-    }
-    html += `
+    let html = ``;
+    let result = document.getElementById("result");
+    for (let x = 0; x < cases.length; x++) {
+        if ((cases[x] + deaths[x] + recovered[x]) == 0) {
+            continue;
+        }
+        html += `
       <tr>
         <td class='td_left'>${cases[x]}</td>
         <td class='td_middle'>${deaths[x]}</td>
@@ -164,12 +179,8 @@ function drawTable() {
         <td class='td_right'>${dates[x]}</td>
       </tr>
       `
-  }
-  result.innerHTML = html;
+    }
+    result.innerHTML = html;
 }
 
 fetchData();
-
-
-
-
--- scripts/index.js
+++ scripts/index.js
@@ -2,14 +2,15 @@ let countries = [];
 let global;
 let data;
 let options = {
-  method: 'GET',
-  mode: 'cors',
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-  },
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+    },
 };
+
 function drawTable(countries) {
-  let html = `
+    let html = `
     <tr>
         <td class='td_left'><a href='country.html?country=Global'>Global</a></td>
         <td class='td_middle'>${global.totalConfirmed}</td>
@@ -19,14 +20,14 @@ function drawTable(countries) {
         <td class='td_right'>${global.newDeaths}</td>
     </tr>
     `;
-  let result = document.getElementById("result");
-  for (let country of countries) {
-    Object.keys(country).forEach(function (key) {
-      if (country[key] === null) {
-        country[key] = '-';
-      }
-    })
-    html += `
+    let result = document.getElementById("result");
+    for (let country of countries) {
+        Object.keys(country).forEach(function(key) {
+            if (country[key] === null) {
+                country[key] = '-';
+            }
+        })
+        html += `
         <tr>
             <td class='td_left'><a href='country.html?country=${country.name}'>${country.name}</a></td>
             <td class='td_middle'>${country.totalConfirmed}</td>
@@ -37,72 +38,76 @@ function drawTable(countries) {
         </tr>
         `;
 
-  }
-  result.innerHTML = html;
+    }
+    result.innerHTML = html;
 }
 
 function search() {
-  let inputField = document.getElementById('country_search');
-  let searchTerm = inputField.value.toUpperCase();
-  let table = document.getElementById('home_table');
-  let tr = table.getElementsByTagName('tr');
-  for (let i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-      txtValue = td.textContent || td.innerText;
-      if (txtValue === '') {
-        fetchTable(url);
-      } else if (txtValue.trim().toUpperCase().includes(searchTerm)) {
-        tr[i].style.display = '';
-      } else {
-        tr[i].style.display = 'none';
-      }
+    let inputField = document.getElementById('country_search');
+    let searchTerm = inputField.value.toUpperCase();
+    let table = document.getElementById('home_table');
+    let tr = table.getElementsByTagName('tr');
+    for (let i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue === '') {
+                fetchTable(url);
+            } else if (txtValue.trim().toUpperCase().includes(searchTerm)) {
+                tr[i].style.display = '';
+            } else {
+                tr[i].style.display = 'none';
+            }
+        }
     }
-  }
 }
 
 function setCountries(data) {
-  global = {
-    'newConfirmed': data.Global.NewConfirmed,
-    'totalConfirmed': data.Global.TotalConfirmed,
-    'newDeaths': data.Global.NewDeaths,
-    'totalDeaths': data.Global.TotalDeaths,
-    'newRecovered': data.Global.NewRecovered,
-    'totalRecovered': data.Global.TotalRecovered,
-  }
-  data.Countries.forEach(e => {
-    let country = {
-      'name': e.Country,
-      'newConfirmed': e.NewConfirmed,
-      'totalConfirmed': e.TotalConfirmed,
-      'newDeaths': e.NewDeaths,
-      'totalDeaths': e.TotalDeaths,
-      'newRecovered': e.NewRecovered,
-      'totalRecovered': e.TotalRecovered,
-
+    global = {
+        'newConfirmed': data.Global.NewConfirmed,
+        'totalConfirmed': data.Global.TotalConfirmed,
+        'newDeaths': data.Global.NewDeaths,
+        'totalDeaths': data.Global.TotalDeaths,
+        'newRecovered': data.Global.NewRecovered,
+        'totalRecovered': data.Global.TotalRecovered,
     }
-    countries.push(country);
-  })
+    data.Countries.forEach(e => {
+        let country = {
+            'name': e.Country,
+            'newConfirmed': e.NewConfirmed,
+            'totalConfirmed': e.TotalConfirmed,
+            'newDeaths': e.NewDeaths,
+            'totalDeaths': e.TotalDeaths,
+            'newRecovered': e.NewRecovered,
+            'totalRecovered': e.TotalRecovered,
+
+        }
+        countries.push(country);
+    })
 }
 
 async function fetchSummary() {
-  try {
-    let response = await fetch('https://api.covid19api.com/summary', options);
-    data = await response.json();
-  } catch (error) {
-    console.log(error);
-  } finally {
-    setCountries(data);
-    updateDate();
-    drawTable(countries);
-  }
+    try {
+        let response = await fetch('https://api.covid19api.com/summary', options);
+        data = await response.json();
+        if (data.Message) {
+            console.log("Caching");
+            alert("API is caching data. Please try again later.");
+        }
+    } catch (error) {
+        console.log(error);
+    } finally {
+        setCountries(data);
+        updateDate();
+        drawTable(countries);
+    }
 }
 
 function updateDate() {
-  const ctx = document.getElementById('update-date');
-  const dateUpdated = data['Date'];
-  let txt = `Last Updated: ${dateUpdated}`;
-  ctx.innerText = txt;
+    const ctx = document.getElementById('update-date');
+    const dateUpdated = data['Date'];
+    let txt = `Last Updated: ${dateUpdated}`;
+    ctx.innerText = txt;
 }
 
 fetchSummary();
